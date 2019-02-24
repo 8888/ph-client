13b05629ee9d4f23f1eef125fdd0265193c5dfa6
betterin30days <leecostello1@gmail.com>
2019-02-24 12:38:10 -0500

    creation of initial property type declarations
    also added a sample json file to mock data

---
+++ src/typings/property.d.ts
@@ -0,0 +1,76 @@
+declare interface Address {
+    street: string;
+    street2?: string;
+    city: string;
+    state: string;
+    zip: string;
+}
+
+declare type PriceHistoryEvent = 
+    "Sold" |
+    "Listed for sale" |
+    "Listing removed" |
+    "Price change"
+
+declare interface PriceHistory {
+    date: string;
+    event: PriceHistoryEvent;
+    price: number;
+}
+
+declare interface TaxHistory {
+    taxAssessment: number;
+    taxes: number;
+    year: number;
+}
+
+declare interface Listing {
+    priceHistory: PriceHistory[];
+    taxHistory: TaxHistory[];
+}
+
+declare interface Note {
+    author: string;
+    body: string;
+    date: string;
+    fromState?: State,
+    state: State;
+}
+
+declare interface PriceAdjustment {
+    amount: number;
+    author: string;
+    date: string;
+    body: string;
+    title: string;
+}
+
+declare interface PropertyFacts {
+    bathrooms: number;
+    bedrooms: number;
+    floorSqft: number;
+    lotSqft: number;
+    type: string;
+    yeaBuilt: number;
+}
+
+declare type State =
+    "null" |
+    "Showing: to be seen" |
+    "Showing: requested" |
+    "Seen" |
+    "Discontinue" |
+    "Investigate" |
+    "Contract: action needed" |
+    "Contract: awaiting response" |
+    "Sale complete" |
+    "Rental unit";
+
+declare interface Property {
+    address: Address;
+    facts: PropertyFacts;
+    listing: Listing;
+    notes: Note[];
+    priceAdjustments: PriceAdjustment;
+    state: State;
+}
